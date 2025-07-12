4d860b2804b74b01dd2416ee9802473f89798a28
YASH TANNA <yashtannasvkd@gmail.com>
2025-07-12 22:16:26 +0530

    Project created

---
+++ backend/src/main/java/com/fullStack/expenseTracker/dto/requests/SignInRequestDto.java
@@ -0,0 +1,47 @@
+package com.fullStack.expenseTracker.dto.requests;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SignInRequestDto {
+    @NotBlank(message = "Email is required!")
+    private String email;
+
+    @NotBlank(message = "Password is required!")
+    private String password;
+
+    
+
+    /**
+     * @return String return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return String return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    
+}
